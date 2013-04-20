9bbbc80b97cbb1bee818c1ced1b28d5b6948fe39
Robert-Emerson <roe2pj@virginia.edu>
2013-04-19 21:02:08 -0400

    Number 4.

--- main.hs
+++ main.hs
@@ -1,6 +1,23 @@
+--A simple fork bomb written in Haskell
+--It is kind enough to make sure you've saved all your work first
+--Author: Robert Emerson
+--Date:	  April 19, 2013
+
+--Import for forkProcess function
 import System.Posix.Process.ByteString
+import System.Posix.Types
+import Data.Either
+
+--data returnType = Either (IO ()) (IO ProcessID)
 
 main = do
-	forkThis
+	putStrLn "Have you saved all your files? (y/n)"
+	response <- getLine
+	parse response
 
-forkThis = putStrLn "Fork you Haskell" >> forkThis
+parse response
+	| response == "y" = forkThis
+	| otherwise = main
+
+forkThis :: IO ProcessID
+forkThis = forkProcess (putStrLn "Fork you Haskell") >> forkThis
